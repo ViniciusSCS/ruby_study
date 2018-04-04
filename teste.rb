5cd9e5fcde38f050ec16b4263f7b92fd9e5f9d67
Vinicius Sarmento <vinicius.siqueira@squadra.com.br>
2018-04-04 10:30:01 -0300

    UPDATE - Estudo sobre métodos no Ruby

--- teste.rb
+++ teste.rb
@@ -355,39 +355,270 @@
 # Teste 19: kind_of?, defined?, all?, select, reject, any?, detect
 #
 
-    puts "=================kind_of?===================="
-    puts "a".kind_of?(String)
+    # puts "=================kind_of?===================="
+    # puts "a".kind_of?(String)
 
-    puts "=================defined??==================="
-    a = "Vinicius"
+    # puts "=================defined??==================="
+    # a = "Vinicius"
 
-    puts defined? a
-    puts defined? 1.next
+    # puts defined? a
+    # puts defined? 1.next
 
-    puts "=================all?========================"
-    a = 2..10
+    # puts "=================all?========================"
+    # a = 2..10
 
-    puts a.all? {|valor| valor > 1}
+    # puts a.all? {|valor| valor > 1}
 
 
-    puts "=================select?====================="
-    a = 1..10
+    # puts "=================select?====================="
+    # a = 1..10
 
-    puts a.select{|valor| valor.even?}
+    # puts a.select{|valor| valor.even?}
     
-    puts "=================reject?====================="
-    a = 1..10
+    # puts "=================reject?====================="
+    # a = 1..10
     
-    puts a.reject{|valor| valor.even?}
+    # puts a.reject{|valor| valor.even?}
     
-    puts "=================any?========================"
-    a = 0..10
+    # puts "=================any?========================"
+    # a = 0..10
 
-    puts a.any? {|valor| valor == 3}
-    puts a.any? {|valor| valor == 11}
+    # puts a.any? {|valor| valor == 3}
+    # puts a.any? {|valor| valor == 11}
 
-    puts "=================detect?====================="
-    a = 0..10
+    # puts "=================detect?====================="
+    # a = 0..10
 
-    puts a.detect {|valor| valor > 0 && valor % 4 == 0}
+    # puts a.detect {|valor| valor > 0 && valor % 4 == 0}
 
+#
+# Teste 20: PREPEND, CENTER, REPLACE, SWAPCASE
+#
+
+    # puts "=====================PREPEND======================"
+    # #inclui caracteres na frente de uma String
+    # a = "Mundo"
+    # puts a.prepend("Olá ")
+
+    # puts "=====================CENTER======================="
+    # #Insere espaços em brancos na esquerda e na direita de uma string
+    # puts "Olá".center(10, '=')
+
+    # puts "=====================REPLACE======================"
+    # #Substitui o conteúdo de uma variável
+    # s = "Olá"
+    # s.replace("Oi")
+    # puts s
+    
+    # puts "=====================SWAPCASE====================="
+    # #Inverte maiusculo e minusculo
+    # puts "oLÁ mUNDO".swapcase
+
+    
+#
+# Teste 21: CLEAR, LSTRIP, RSTRIP, EACH_CHAR
+#
+
+    # puts "=====================CLEAR==================="
+    # # Este método elimina uma string
+    # a = "mundo"
+    # puts a.clear
+
+    # puts "=====================LSTRIP=================="
+    # # Elimina caracteres em banco à esquerda
+    # puts "                  olá".lstrip
+
+
+    # puts "=====================RSTRIP=================="
+    # # Elimina caracteres em branco à direita
+    # puts "olá                     ".rstrip
+
+    # puts "=====================EACH_CHAR==============="
+    # # Percorre cada caracter da String
+    # puts "=====Usando split====="
+    # a = "mundo"
+    # b = a.split("")
+    # for c in b
+    #     puts c
+    # end
+    
+    # puts "=====Usando each_char====="
+
+    # a.each_char do |caracter|
+    #     puts caracter
+    # end
+
+#
+# Teste 22: MIN, MAX, INJECT, PARTITION, ZIP, ARITY, EACH_WITH_INDEX
+#
+
+    # puts "===================MIN======================"
+    # puts v = [3,3,9,8,4,3,8,4,9,7,1,2].min
+
+    # puts "===================MAX======================"
+    # puts v = [3,3,9,8,4,3,8,4,9,7,1,2].max
+
+    # puts "===================INJECT==================="
+    # a = 0..10
+
+    # puts a.inject(:+)
+
+    # puts "===================PARTITION================"
+    # a = 0..10
+    # v = a.partition do |valor|
+    #     valor.even?
+    # end
+
+    # puts v.inspect
+
+    # puts "===================ZIP======================"
+    # a = [1, 2]
+    # b = [3, 4]
+
+    # print a.zip(b)
+    # puts
+    # puts a.zip(b)
+
+    # puts "===================ARITY===================="
+    # def soma(n1, n2)
+    #     n1 + n2
+    # end
+    
+    # puts "O método recebe #{method(:soma).arity} parâmetros"
+    
+    # puts "===================EACH_WITH_INDEX=========="
+    # a = ["a", "g", 1, 2, 3, "b", "c", 6, "o", "l"]
+
+    # a.each_with_index do |item, indice|
+    #     puts "O item #{item} está no indice #{indice}"
+
+    # end
+
+#
+# Teste 23: EVAL, INSTANCE_OF?, GSUB, ARGV, CONSTANTES
+#
+
+    # puts "================EVAL============================="
+    # n = "1 + 2"
+    # y = eval(n)
+    
+    # puts y
+
+    # puts "================INSTANCE_OF?====================="
+    # # Descobre de que instância é o objeto
+    # x = 1
+
+    # puts x.instance_of?(String)
+    # puts x.instance_of?(Array)
+    # puts x.instance_of?(Fixnum)
+    
+    # puts
+
+    # def exibir(y)
+    #     if y.instance_of?(String)
+    #         puts "Sou uma String"
+    #     elsif y.instance_of?(Fixnum)
+    #         puts "Sou um Fixnum"
+    #     elsif y.instance_of?(Array)
+    #         puts "Sou um Array"
+    #     else
+    #         puts "--------"
+    #     end
+    # end
+
+    # exibir("A, b, c")
+
+    # puts "================GSUB============================="
+    # # Substituir Frase
+    # frase = "Olá mundo"
+
+    # frase = frase.gsub!("mundo", "Vinicius")
+
+    # puts frase
+    # puts "================ARGV============================="
+    # # Vetor do Sistema
+    # v = ARGV
+
+    # puts v.inspect
+    # puts "================Concatenando com <<=============="
+    
+    # puts "Objeto 'a' normal."
+    # a = "Olá"
+    # puts a
+    # puts a.object_id
+    # puts
+    # puts
+    # puts "Objeto 'a' concatenado com (+)"
+    # a = a + " Mundo"
+    # puts a
+    # puts a.object_id
+    # puts "Objeto 'a' concatenado com (<<)"
+    # a = a << "!!!"
+    # puts a
+    # puts a.object_id
+
+    # puts "================CONSTANTES======================="
+    # # Quando modificados geram uma mensagem de alerta
+    # puts Math::PI
+
+    # TESTE = 1 #Constante sempre com letras maiúsculas
+    # TESTE = 2
+
+    # puts TESTE
+
+#
+# Teste 24: nil?, dup, freeze, scan, respond_to?, to_a, conversões de bases
+#
+
+    # puts "====================nil?==========================="
+    # # Verifica se a variável está vazia
+    # v = nil
+
+    # puts v.nil?
+    # puts "====================dup============================"
+    # # Copia uma variável
+    # nome = "Vinicius"
+
+    # outro_nome = nome.dup
+    
+    # nome[0] = "x"
+
+    # puts nome
+    # puts
+    # puts outro_nome
+    # puts "====================freeze========================="
+    # # Congela um objeto
+    # nome = "Vinicius"
+    # nome.freeze
+
+    # nome[0] = "x"
+
+    # puts nome
+
+    # puts "====================scan==========================="
+    # # Procura letras
+    # str = "4b0b0r4"
+
+    # a = str.scan("b")
+
+    # puts a.size
+
+    # puts "====================respond_to?===================="
+    # # Verifica se um objeto suporta um determinado método
+    # puts 1.respond_to?(:<<)
+    # puts 1.respond_to?(:xxxx)
+    # puts 1.respond_to?(:join)
+    # puts 1.respond_to?(:to_s)
+    # puts 1.respond_to?(:split)
+    # puts "====================to_a==========================="
+    # # Converte um range em array
+    # print ("a".."z").to_a
+    # puts
+
+    # puts "====================conversões de bases (to_s)============"
+    # # Inteiro para binário
+    # puts 15.to_s(16)
+
+#
+# Teste 25: Métodos para Array
+#
